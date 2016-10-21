ee3ff380c78d2298f10ae0408f34216bdfbcc63e
Thomas Foster <thomasfoster90>
2016-10-21 11:47:08 -0500

    mangodb

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const mongoose + require('mongoose');
 
+
+
+mangoose.connect(process.env.DB_CONNECTION)
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
